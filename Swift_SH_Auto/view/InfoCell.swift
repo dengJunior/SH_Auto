95e422a47107f149339ab2f45c2040f76a3486dc
gaokunpeng <gaokunpeng@gaokunpeng-2.local>
2015-12-15 21:23:12 +0800

    详情页

--- Swift_SH_Auto/view/InfoCell.swift
+++ Swift_SH_Auto/view/InfoCell.swift
@@ -13,6 +13,8 @@ class InfoCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
