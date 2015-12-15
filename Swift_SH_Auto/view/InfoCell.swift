062fbe669a946a29744c23d1bec6645ffca5e7a1
gaokunpeng <gaokunpeng@gaokunpeng-2.local>
2015-12-15 12:13:21 +0800

    cell

---
+++ Swift_SH_Auto/view/InfoCell.swift
@@ -0,0 +1,24 @@
+//
+//  InfoCell.swift
+//  Swift_SH_Auto
+//
+//  Created by gaokunpeng on 15/12/15.
+//  Copyright © 2015年 apple. All rights reserved.
+//
+
+import UIKit
+
+class InfoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
